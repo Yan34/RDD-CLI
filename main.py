a2588525aa4cc283e9702ea643e94fb5db496805
Yan Furman <furmanyan@gmail.com>
2023-06-04 02:07:52 +0300

    Добавлен обработчик аргументов командной строки

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ from YoloV8Detector import YoloV8Detector
 import argparse
 
 
+mrcnn_config_path = os.path.abspath('models/mrcnn/config_3.py')
+mrcnn_model_path = os.path.abspath('models/mrcnn/3_epoch_9.pth')
+yolov8_model_path = os.path.abspath('models/yolov8/train20.pt')
+
+
+def createParser ():
+    parser_ = argparse.ArgumentParser()
+    parser_.add_argument('-d', '--detector', choices=['mrcnn', 'yolov8'], required=True)
+    parser_.add_argument('-m', '--mode', choices=['image', 'video'], required=True)
+    parser_.add_argument('-f', '--file', required=True, type=str)
+    return parser_
+
+
 if __name__ == '__main__':
-    detector = MRCNNDetector(os.path.abspath('models/mrcnn/config_3.py'), os.path.abspath('models/mrcnn/3_epoch_9.pth'))
-    # detector = YoloV8Detector(os.path.abspath('models/yolov8/train20.pt'))
-    # detector.detect("D:\\University\\RoadDamageDetection\\DatasetCocoFormat1200\\test\\819785_ES_259_259ES000000_00410_RAW.jpg", 'image')
-    detector.detect("D:\\University\\АД\\Кузьмичи\\18 ОП РЗ 18К-01-16 (прямое направление).avi", 'video')
+    parser = createParser()
+    args = parser.parse_args()
+    mode = args.mode
+    path = os.path.abspath(args.file)
+    if args.detector == 'mrcnn':
+        detector = MRCNNDetector(mrcnn_config_path, mrcnn_model_path)
+    elif args.detector == 'yolov8':
+        detector = YoloV8Detector(yolov8_model_path)
+    else:
+        print('Детектор не опознан')
+        exit()
+    detector.detect(path, mode)
